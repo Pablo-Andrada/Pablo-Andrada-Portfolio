2f18fc0d90c3824412f69d7473cb9c05d3064ad4
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-12-18 03:10:16 -0300

    vercel uploading 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel'
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server', // Necesario para Vercel
+  adapter: vercel(), // Usa el adaptador de Vercel
 });
